4f6153f768291997bd9090da35ce7a01b4509d29
Claire DeBoer <claire.deboer@gmail.com>
2020-10-30 21:14:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = 0
+  # min_value = 100
+  # min_key = ""
+  array = name_hash.to_a
+  min_key = array[0][0]  # apple
+  min_value = array[0][1] #-45
+
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
- end   
- key
+      min_key = key
+    end   
+  end
+  min_key
 end
