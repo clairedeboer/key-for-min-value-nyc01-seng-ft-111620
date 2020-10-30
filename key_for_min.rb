b6988c8b03821b63b33de902efda08ae15abb7e2
Claire DeBoer <claire.deboer@gmail.com>
2020-10-30 21:18:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,8 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash.empty? 
-  # min_value = 100
-  # min_key = ""
-  array = name_hash.to_a
-  min_key = array[0][0]  # apple
-  min_value = array[0][1] #-45
-
+  min_value = 100
+  min_key = ""
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
